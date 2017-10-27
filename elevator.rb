8bccb0730606736a828ab41b6fa3250474e5bbf9
Christi Ovchinnikov Barker <kaco7777@gmail.com>
2017-10-26 22:52:00 -0600

    indentation

--- elevator.rb
+++ elevator.rb
@@ -7,7 +7,6 @@ class Elevator
 		if @floor < 12
 			@floor += 1
 		end
-		
 	end
 	def go_down()
 		if @floor > 1
@@ -17,23 +16,21 @@ class Elevator
 	def greeting()
 		puts "welcome to the #{floor} floor"
 	end
-	
-	
 end
 
 el = Elevator.new(9)
-puts el.greeting
-el.go_down
-puts el.greeting
-15.times do 
+	puts el.greeting
 	el.go_down
-end
-puts el.greeting
-4.times do
-	el.go_up
-end
-puts el.greeting
-12.times do
-	el.go_up
-end
-puts el.greeting
+	puts el.greeting
+	15.times do 
+		el.go_down
+	end
+	puts el.greeting
+	4.times do
+		el.go_up
+	end
+	puts el.greeting
+	12.times do
+		el.go_up
+	end
+	puts el.greeting
