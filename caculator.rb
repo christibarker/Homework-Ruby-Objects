5c8a6c6162745420de39da848497c9d03e546da8
Christi Ovchinnikov Barker <kaco7777@gmail.com>
2017-11-02 22:17:51 -0600

    caculator

--- caculator.rb
+++ caculator.rb
@@ -1,30 +1,25 @@
-class Caculator
+class Calculator
 	def initialize()
 	end
-	def add(value1, value2)
-		if value1 + value2
-			puts value1 + value2
-		end
+
+	def self.add(value1, value2)
+			value1 + value2	
 	end
-	def subtract(value1, value2)
-		if value1 - value2
-			puts value1 - value2
-		end
+
+	def self.subtract(value1, value2)
+			value1 - value2
 	end
-	def multiply(value1, value2)
-		if value1 * value2
-			puts value1 * value2
-		end
+
+	def self.multiply(value1, value2)
+			value1 * value2
 	end
-	def divide(value1, value2)
-		if value1 / value2
-			puts value1 / value2
-		end
+
+	def self.divide(value1, value2)
+			value1 / value2
 	end
 end
 
-cal = Caculator.new
-	puts cal.add(50, 63)
-	puts cal.subtract(3, 4)
-	puts cal.multiply(47, 3)
-	puts cal.divide(100, 60)
+puts Calculator.add(50,63)
+puts Calculator.subtract(10,5)
+puts Calculator.multiply(47, 3)
+puts Calculator.divide(100, 60)
