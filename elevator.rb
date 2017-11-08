4d0d2c0b2e890fe160365cec293e539619536cf6
Christi Ovchinnikov Barker <kaco7777@gmail.com>
2017-11-08 09:52:00 -0700

    elevator

--- elevator.rb
+++ elevator.rb
@@ -1,36 +1,37 @@
 class Elevator
 	attr_accessor :floor
-	def initialize(args)
-		@floor = args
+	def initialize()
+		@floor = 1
 	end
 	def go_up()
 		if @floor < 12
 			@floor += 1
 		end
+		self.greeting
 	end
 	def go_down()
 		if @floor > 1
 			@floor -= 1 
 		end
+		self.greeting
 	end
 	def greeting()
 		puts "welcome to the #{floor} floor"
 	end
 end
 
-el = Elevator.new(9)
-	puts el.greeting
+el = Elevator.new()
 	el.go_down
-	puts el.greeting
+
 	15.times do 
 		el.go_down
 	end
-	puts el.greeting
+	
 	4.times do
 		el.go_up
 	end
-	puts el.greeting
+
 	12.times do
 		el.go_up
 	end
-	puts el.greeting
+	
